f4df7e1137e6aeda3e8baccdc9d78188dc5a7c63
Mattias Soroccos <matte_1500@hotmail.com>
2018-02-21 15:36:43 +0100

    gvgvbh

--- PragueParkingDuo/PraugeParkingGeneralTest/ParkingLotTest.cs
+++ PragueParkingDuo/PraugeParkingGeneralTest/ParkingLotTest.cs
@@ -17,21 +17,45 @@ namespace PraugeParkingGeneralTest
             // Assign
             ParkingLot testLot = new ParkingLot();
             ParkingSlot testSlot;
+            Car testCar = new Car("ABC123");
             // Act
-            testLot.TryAddVehicle<Car>("ABC123",out int temp);
-            testSlot = testLot.Content()[0];
+            testLot.TryAddVehicle(testCar, out int temp);
+            testSlot = (ParkingSlot)testLot.Content().GetValue(0);
             // Assert
-
+            Assert.IsTrue(testSlot.Content().Contains(testCar));
         }
 
         [TestMethod]
-        public void TryAddVehicle_AddedVehicleToPartialyFullLot_IsTrue()
+        public void TryAddVehicle_AddedVehiclesToPartialyFullLot_IsTrue()
         {
             // Assign
-
+            ParkingLot testLot = new ParkingLot();
+            ParkingSlot[] testSlots = new ParkingSlot[4];
+            Vehicle[] vehList = new Vehicle[6];
+            int actualValue = 0;
+            int expectedValue = 6;
+            
+            vehList[0] = new Car("A1");
+            vehList[1] = new Mc("A2");
+            vehList[2] = new Mc("A4");
+            vehList[3] = new Trike("A5");
+            vehList[4] = new Bike("A6");
+            vehList[5] = new Bike("A7");
+            int temp;
             // Act
-
+            testLot.TryAddVehicle(vehList[0], out temp);
+            testLot.TryAddVehicle(vehList[1], out temp);
+            testLot.TryAddVehicle(vehList[2], out temp);
+            testLot.TryAddVehicle(vehList[3], out temp);
+            testLot.TryAddVehicle(vehList[4], out temp);
+            testLot.TryAddVehicle(vehList[5], out temp);
+            for(int i = 0; i < 4; i++)
+            {
+                testSlots[i] = (ParkingSlot)testLot.Content().GetValue(i);
+            }
             // Assert
+            
+            
         }
 
         [TestMethod]
